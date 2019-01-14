8c811806a4aa1f25c87d5471b7405bcd7d368172
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-14 16:23:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,8 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts ("   |   |   ")
-  puts ("-----------")
+  puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
+puts ("-----------")
   puts ("   |   |   ")
   puts ("-----------")
   puts ("   |   |   ")
