229ad1f4b1d9ef4369fa82a1430bbcc31bf18ead
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-14 15:46:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board)
+def display_board
   puts ("   |   |   ")
   puts ("-----------")
   puts ("   |   |   ")
@@ -11,7 +11,7 @@ end
 end
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board()
+def display_board(board)
   puts ("   |   |   ")
   puts ("-----------")
   puts ("   | X |   ")
